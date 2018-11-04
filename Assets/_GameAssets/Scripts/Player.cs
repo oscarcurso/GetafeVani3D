85331c9b7c4d15fd833615f318b57679c7a7705d
oscarcurso <43536073+oscarcurso@users.noreply.github.com>
2018-11-04 20:46:04 +0100

    +

--- Assets/_GameAssets/Scripts/Player.cs
+++ Assets/_GameAssets/Scripts/Player.cs
@@ -56,6 +56,7 @@ public class Player : MonoBehaviour {
     private bool EstaEnElSuelo() {
         bool enSuelo=false;
         Collider2D col = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
+        //Collider col3 = Physics.OverlapSphere(posPies.position, 1);
         if(col != null) {
             enSuelo = true;
         }
